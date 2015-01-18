1597ce067881fd8d50aba36fe063d8dd5ccceb4e
sufianj <sufianj@users.noreply.github.com>
2015-01-18 23:34:51 +0100

    Update update.java

--- update.java
+++ update.java
@@ -1,24 +1,29 @@
-protected void update(Preference preference, double mu) {
-    int userIndex = userIndex(preference.getUserID());
-    int itemIndex = itemIndex(preference.getItemID());
+protected void updateParameters(long userID, long itemID, float rating, double currentLearningRate) {
+    int userIndex = userIndex(userID);
+    int itemIndex = itemIndex(itemID);
 
     double[] userVector = userVectors[userIndex];
     double[] itemVector = itemVectors[itemIndex];
+    double prediction = predictRating(userIndex, itemIndex);
+    double err = rating - prediction;
 
-    double prediction = dot(userVector, itemVector);
-    double err = preference.getValue() - prediction;
+    // adjust user bias
+    userVector[USER_BIAS_INDEX] +=
+        biasLearningRate * currentLearningRate * (err - biasReg * preventOverfitting * userVector[USER_BIAS_INDEX]);
+
+    // adjust item bias
+    itemVector[ITEM_BIAS_INDEX] +=
+        biasLearningRate * currentLearningRate * (err - biasReg * preventOverfitting * itemVector[ITEM_BIAS_INDEX]);
 
     // adjust features
-    for (int k = FEATURE_OFFSET; k < rank; k++) {
-      double userFeature = userVector[k];
-      double itemFeature = itemVector[k];
+    for (int feature = FEATURE_OFFSET; feature < numFeatures; feature++) {
+      double userFeature = userVector[feature];
+      double itemFeature = itemVector[feature];
 
-      userVector[k] += mu * (err * itemFeature - lambda * userFeature);
-      itemVector[k] += mu * (err * userFeature - lambda * itemFeature);
+      double deltaUserFeature = err * itemFeature - preventOverfitting * userFeature;
+      userVector[feature] += currentLearningRate * deltaUserFeature;
+
+      double deltaItemFeature = err * userFeature - preventOverfitting * itemFeature;
+      itemVector[feature] += currentLearningRate * deltaItemFeature;
     }
-
-    // adjust user and item bias
-    userVector[USER_BIAS_INDEX] += biasMuRatio * mu * (err - biasLambdaRatio * lambda * userVector[USER_BIAS_INDEX]);
-    itemVector[ITEM_BIAS_INDEX] += biasMuRatio * mu * (err - biasLambdaRatio * lambda * itemVector[ITEM_BIAS_INDEX]);
-    //System.out.printf("for (u=%d, i=%d) userbias = %f, itembias = %f \n",userIndex, itemIndex, userVector[USER_BIAS_INDEX],itemVector[ITEM_BIAS_INDEX] );
   }
